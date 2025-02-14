b6681c9c9276384f48cd8aebef48e8b1241be2a7
kimnino <mh8375@naver.com>
2025-02-14 23:28:50 +0900

    feat : 실습내용

---
+++ section05/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
+
+function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
+  return (
+    <>
+      <Button {...buttonProps} />
+      <Button text={"카페"}>
+        <Header />
+      </Button>
+      <Button text={"블로그"}>
+        <div>자식 요소</div>
+      </Button>
+    </>
+  );
+}
+
+export default App;

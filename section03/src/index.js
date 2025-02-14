5dda00c2cd9b216301eb30588da794d61aa71834
kimnino <mh8375@naver.com>
2025-02-14 17:26:21 +0900

    feat : node js 강의 실습 내용

---
+++ section03/src/index.js
@@ -0,0 +1,10 @@
+import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+//console.log(add(10, 10));
+//console.log(sub(20, 19));
+//console.log(mul(2, 3));

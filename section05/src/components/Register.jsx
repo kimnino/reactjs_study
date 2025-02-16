77d68b635c85d4efd0253015f2adc3e32ec13dd9
kimnino <mh8375@naver.com>
2025-02-16 22:25:17 +0900

    feat : 리액트 실습 내용

---
+++ section05/src/components/Register.jsx
@@ -0,0 +1,75 @@
+import { useState, useRef } from "react";
+// 간단한 회원가입
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+let count = 0;
+
+const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
+  const onChange = (e) => {
+    //countRef.current++;
+    count++;
+    console.log(count);
+    setInput({
+      // 객체를 선언할 때, 대괄호 안에 변수명을 쓰면 키값이 됩니다.
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={input.birth}
+          type="date"
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+
+      <button onClick={onSubmit}>제출</button>
+    </div>
+  );
+};
+
+export default Register;

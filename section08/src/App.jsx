b2e11dd103aa02a2e105909e75b1c2d9b9339cc7
kimnino <mh8375@naver.com>
2025-03-23 23:31:25 +0900

    feat : 강의 내용

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,14 +1,63 @@
 import "./App.css";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Java 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "Spring 공부하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    // todos.push(newTodo); 절대안된다.
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
